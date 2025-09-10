4771efd4e94d7b8a91f2d253e315c1b9f98c4835
405supply <405supplychoi@gmail.com>
2025-09-10 16:15:54 +0900

    Chap06 added

---
+++ src/Chap06BookDto.java
@@ -0,0 +1,69 @@
+public class Chap06BookDto {
+    private String title;
+    private String publisher;
+    private String author;
+    private int price;
+    private double discountRate;
+
+    Chap06BookDto() {};
+
+    Chap06BookDto(String title, String publisher, String author) {
+        this.title = title;
+        this.publisher = publisher;
+        this.author = author;
+    }
+
+    Chap06BookDto(String title, String publisher, String author, int price, double discountRate) {
+        this.title = title;
+        this.publisher = publisher;
+        this.author = author;
+        this.price = price;
+        this.discountRate = discountRate;
+    }
+
+    public void printInfo() {
+        System.out.println(title + " " + publisher + " " + author + " " + price + " " + discountRate);
+    }
+
+
+    // Getter & Setter
+    public String getTitle() {
+        return title;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public double getDiscountRate() {
+        return discountRate;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public void setDiscountRate(double discountRate) {
+        this.discountRate = discountRate;
+    }
+}
